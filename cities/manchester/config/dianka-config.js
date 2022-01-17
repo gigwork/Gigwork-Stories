5af9842d25b0063c00c16e4d437d04f8d754081f
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2022-01-17 23:03:51 +0000

    adding new txt files

--- cities/manchester/config/dianka-config.js
+++ cities/manchester/config/dianka-config.js
@@ -1,6 +1,6 @@
 // Number of slides that will drive (more = smoother)
 // If this doesn't match the number of slides named 'drive-slide' in config below you will not complete the full journey
-var driveSlides = 7;
+var driveSlides = 9;
 
 // Number of points on drive route (more = higher quality, but slower to process)
 var driveSmoothness = 200;
@@ -34,7 +34,7 @@ var config = {
     chapters: [{
             id: 'slide-0',
             title: 'Dianka',
-            description: '<b>Location:</b> Manchester </br> <b>Date:</b> Saturday 27/03/2020 </br> <b>Platform:</b> Uber Eats </br> </br> <b>Bio: </br> </br> <h3 style="text-align:center"> Art by <a href="#">Rafeal</a> </h3>',
+            description: '<b>Location:</b> Manchester </br> <b>Date:</b> Tuesday 11/02/2021 </br> <b>Platform:</b> Deliveroo & Uber Eats </br> </br> <b>Bio:</b> 31 years old Dianka is one of the few women doing food deliveries in Manchester. Originally from Czech Republic, which she left six years ago, Dianka has a bachelors’ degree in economics and management that wasn’t of much use once arrived in the UK. Instead, she’s been doing warehouse, cleaning and hospitality jobs for which she was often paid less than the minimum wage. While she initially started doing deliveries on an e-bike as a side job, it quickly became a full-time enterprise out of necessity. It was both the covid pandemic and a partner with medical problems who needed constant looking after which forced her into a more flexible job. As many other couriers, she’s ‘multiapping’, doing Deliveroo and Uber Eats at the same time in order to earn enough money. While the delivery money is good to cover for everyday expenses, being a woman comes at a hefty price as she receives unwarranted attention from men, who are often colleagues at work. As a result, she always carries a pepper spray, a ‘present’ she’s received from her partner’s sister. Outside work, she loves nature and spends as much time as she can in Drinkwater Park, north of Manchester.</br> <h3 style="text-align:center"> Art by <a href="#">Rafeal</a> </h3>',
             location: {},
             earnings: '',
             onChapterEnter: [],
@@ -42,9 +42,9 @@ var config = {
         },
         {
             id: 'drive-slide-0',
-            title: '10:36AM Start of the day',
+            title: '06:59AM An early start',
             audio: '../../riders/audio/Dianka/1.m4a',
-            description: '<a href="#"> Read Transcript </a> ',
+            description: '<a href="../../riders/transcript/dianka/dianka.txt"> Read Transcript </a>',
             location: {},
             earnings: '0',
             onChapterEnter: [],
@@ -60,9 +60,9 @@ var config = {
         },
         {
             id: 'drive-slide-1',
-            title: '11:33AM Four orders later',
+            title: '07:29:AM A glitch in the app',
             audio: '../../riders/audio/Dianka/2.m4a',
-            description: '<a href="#"> Read Transcript </a> ',
+            description: '<a href="../../riders/transcript/dianka/dianka1.txt"> Read Transcript </a>',
             location: {},
             earnings: '15.32',
             onChapterEnter: [],
@@ -70,7 +70,16 @@ var config = {
         },
         {
             id: 'drive-slide-2',
-            title: '',
+            title: '08:41:AM A fragile delivery and some merchanical problems',
+            audio: '../../riders/audio/Dianka/3.m4a',
+            description: '<a href="../../riders/transcript/dianka/dianka2.txt"> Read Transcript </a>',
+            location: {},
+            earnings: '15.32',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'slide-2',
             image: '../../riders/artwork/Dianka/Dianka2.jpg',
             description: '',
             location: {},
@@ -80,9 +89,9 @@ var config = {
         },
         {
             id: 'drive-slide-3',
-            title: '12:17PM Less than the minimum wage',
-            audio: '../../riders/audio/Dianka/3.m4a',
-            description: '<a href="#"> Read Transcript </a> ',
+            title: '11:32AM A break, a hot shower and back at work',
+            audio: '../../riders/audio/Dianka/4.m4a',
+            description: '<a href="../../riders/transcript/dianka/dianka3.txt"> Read Transcript </a>',
             location: {},
             earnings: '22.86',
             onChapterEnter: [],
@@ -90,29 +99,45 @@ var config = {
         },
         {
             id: 'drive-slide-4',
-            title: '13:28AM A very quiet day',
-            audio: '../../riders/audio/Dianka/4.m4a',
-            description: '<a href="#"> Read Transcript </a> ',
+            title: '13:00PM Busy Uber, quiet Deliveroo',
+            audio: '../../riders/audio/Dianka/5.m4a',
+            description: '<a href="../../riders/transcript/dianka/dianka4.txt"> Read Transcript </a>',
             location: {},
             earnings: '25.97',
             onChapterEnter: [],
             onChapterExit: []
         },
         {
+            id: 'slide-3',
+            image: '../../riders/artwork/Dianka/Dianka3.jpg',
+            description: '',
+            location: {},
+            earnings: '19.75',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
             id: 'drive-slide-5',
-            title: '13:30PM Its good to speak French',
+            title: '13:53PM Lunch break, games and bike fixing',
             audio: '../../riders/audio/Dianka/5.m4a',
-            description: '<a href="#"> Read Transcript </a> ',
+            description: '<a href="../../riders/transcript/dianka/dianka5.txt"> Read Transcript </a>',
             earnings: '30.15',
             location: {},
             onChapterEnter: [],
             onChapterExit: []
         },
-        // BREAK
         {
             id: 'slide-4',
-            title: '14:01PM  A break to charge the batteries',
-            description: 'Dianka signs off Uber Eats',
+            description: 'Dianka signs out of Deliveroo',
+            location: {},
+            earnings: '19.75',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        // BREAK
+        {
+            id: 'drive-slide-6',
+            title: '19:01PM More hot showers, more clothes',
             audio: '../../riders/audio/Dianka/6.m4a',
             description: '<a href="#"> Read Transcript </a> ',
             earnings: '30.15',
