720d0c37402d3a4e923b31d8d3efa080f8f8d227
natesheehan <nathanaelsheehan@gmail.com>
2023-01-30 00:22:44 +0000

    update folders

--- __/firebase/init.js
+++
@@ -1,9 +0,0 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
-firebase.initializeApp({
-  "apiKey": "AIzaSyDrGIqhifUQfMRKxQ2pRprUTOaAO5R5lvM",
-  "authDomain": "gigwork-d64b8.firebaseapp.com",
-  "databaseURL": "",
-  "messagingSenderId": "1036983944182",
-  "projectId": "gigwork-d64b8",
-  "storageBucket": "gigwork-d64b8.appspot.com"
-});
