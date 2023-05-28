070cf04228a71c9dde2cde0157372f498b18e4fd
Sheehan <ns651@exeter.ac.uk>
2023-05-28 13:56:53 +0100

    add ismael config

--- cities/lyon/config/ismael-config.js
+++ cities/lyon/config/ismael-config.js
@@ -23,14 +23,14 @@ var config = {
             id: 'slide-0',
             title: 'Ismael',
             image: '../../riders/artwork/smael/Ismael 1.jpg',
-            description: '<b>Location:</b> Lyon </br> <b>Date:</b> Saturday 30/07/2022 and Saturday 20/08/2022 </br> <b>Platform:</b> Uber Eats </br> </br> Ismael is a 31-year-old refugee from a small village in south-east Sudan. He arrived in France, but his dream was to find work in neighbouring Libya. He crossed the border separating Sudan from the war-ravaged country in 2015, hoping to earn money to buy a tractor and farming equipment for his plot of land back home. ‘We are all peasants where I am coming from. We have guava and mango trees, watermelons, vegetables, and cereals. Everyone has their small farm, but we don’t have the tools’, he tells me. After two years in Libya, it was no longer safe to return to Sudan, so Ismael decided to cross the Mediterranean to try and reach Europe. Following a few tough months spent in the makeshift camp in Calais, he eventually arrived in Lyon. While waiting for his asylum claim, he started volunteering for Red Cross and Amnesty International to improve his French and integrate in the city. He lives now in a small town 60 km outside Lyon where he is following a training programme to become an electric engineer. In the meantime, he spends his weekends visiting friends in Lyon where he often does deliveries for Uber Eats to top up his meagre income. I followed Ismael at work in the torrid summer of 2022: the orders were scarce and far away. At a point we even had to take a metro to complete a 5 km order to Vaulx-en-Velin, in the outskirts of the city. </br> </br> <h3 style="text-align:center"> Art by <a target="_blank" href="https://www.instagram.com/ionut_dulamita/"">Ionuț Dulămiță</a> </h3>',
+            description: '<b>Location:</b> Lyon </br> <b>Date:</b> ? </br> <b>Platform:</b> Uber Eats </br> </br> Ismael is a 31-year-old refugee from a small village in south-east Sudan. He arrived in France, but his dream was to find work in neighbouring Libya. He crossed the border separating Sudan from the war-ravaged country in 2015, hoping to earn money to buy a tractor and farming equipment for his plot of land back home. ‘We are all peasants where I am coming from. We have guava and mango trees, watermelons, vegetables, and cereals. Everyone has their small farm, but we don’t have the tools’, he tells me. After two years in Libya, it was no longer safe to return to Sudan, so Ismael decided to cross the Mediterranean to try and reach Europe. Following a few tough months spent in the makeshift camp in Calais, he eventually arrived in Lyon. While waiting for his asylum claim, he started volunteering for Red Cross and Amnesty International to improve his French and integrate in the city. He lives now in a small town 60 km outside Lyon where he is following a training programme to become an electric engineer. In the meantime, he spends his weekends visiting friends in Lyon where he often does deliveries for Uber Eats to top up his meagre income. I followed Ismael at work in the torrid summer of 2022: the orders were scarce and far away. At a point we even had to take a metro to complete a 5 km order to Vaulx-en-Velin, in the outskirts of the city. </br> </br> <h3 style="text-align:center"> Art by <a target="_blank" href="https://www.instagram.com/ionut_dulamita/"">Ionuț Dulămiță</a> </h3>',
             location: {},
             onChapterEnter: [],
             onChapterExit: []
         },
         {
             id: 'drive-slide-0',
-            title: '10.10:Today we are planning to work hard',
+            title: '12.36: Its hot, so its very difficult',
             audio: '../../riders/audio/Ismael/1.m4a',
             description: '<a target="_blank" href="../../riders/transcript/Ismael.txt"> Read Transcript </a> ',
             location: {},
@@ -40,11 +40,11 @@ var config = {
         },
         {
             id: 'drive-slide-1',
-            title: '12.13:Let them give us more orders',
+            title: '13.32: White zones are areas where we do not receive orders',
             audio: '../../riders/audio/Ismael/2.m4a',
             description: '<a target="_blank" href="../../riders/transcript/Ismael.txt"> Read Transcript </a> ',
             location: {},
-            earnings: '€7.74',
+            earnings: '€9.04',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -52,46 +52,63 @@ var config = {
             id: 'slide-1',
             image: '../../riders/artwork/smael/Ismael 2.jpg',
             location: {},
-            earnings: '€7.74',
+            earnings: '€16.42',
             onChapterEnter: [],
             onChapterExit: []
         },
         {
             id: 'drive-slide-2',
-            title: '13.58: Many people do not have money at the end of the month',
+            title: '14.13: More shadows, but no orders',
             audio: '../../riders/audio/Ismael/3.m4a',
             description: '<a target="_blank" href="../../riders/transcript/Ismael.txt"> Read Transcript </a> ',
             location: {},
-            earnings: '€20.48',
+            earnings: '€28.12',
             onChapterEnter: [],
             onChapterExit: []
         },
         {
             id: 'slide-4',
             title: '',
+            earnings: '€28.12',
+            image: '../../riders/data/Lyon/Ismael Day 1.jpg',
+            summaryzoom: 10,
+            location: {
+                center: [-2.244644, 53.483959],
+                zoom: 15.23,
+                pitch: 60.00,
+                bearing: 43.61
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+
+
+        {
+            id: 'slide-4',
+            title: 'Day 2',
             image: '../../riders/artwork/smael/Ismael 3.jpg',
             description: '',
             location: {},
-            earnings: '€20.48',
+            earnings: '€0',
             onChapterEnter: [],
             onChapterExit: []
         },
         {
             id: 'drive-slide-3',
-            title: '16.21:I come here to charge batteries and collect food',
+            title: '11.59: My bike was stolen, so I couldnt work for two days',
             audio: '../../riders/audio/Ismael/4.m4a',
             description: '<a target="_blank" href="../../riders/transcript/Ismael.txt"> Read Transcript </a> ',
             location: {},
-            earnings: '€24.82',
+            earnings: '€0',
             onChapterEnter: [],
             onChapterExit: []
         },
         {
             id: 'drive-slide-4',
-            title: '20.06:I dont know if youre tired, but we can work until 11',
+            title: '12.49: You need to move a lot, even during a heatwave ',
             audio: '../../riders/audio/Ismael/5.m4a',
             description: '<a target="_blank" href="../../riders/transcript/Ismael.txt"> Read Transcript </a> ',
-            earnings: '€34.73',
+            earnings: '€4.20',
             location: {},
             onChapterEnter: [],
             onChapterExit: []
@@ -99,29 +116,29 @@ var config = {
         {
             id: 'slide-5',
             title: '',
-            image: '../../riders/artwork/smael/Ismael 4.jpg',
+            image: '../../riders/artwork/smael/Ismael 4.jpeg',
             description: '',
             location: {},
-            earnings: '€34.73',
+            earnings: '€8.29.',
             onChapterEnter: [],
             onChapterExit: []
         },
         {
             id: 'drive-slide-5',
-            title: '21.46: 36% on the phone and 3 on the bike.',
+            title: '15.15: 13 euros in four hours.',
             audio: '../../riders/audio/Ismael/6.m4a',
             description: '<a target="_blank" href="../../riders/transcript/Ismael.txt"> Read Transcript </a> ',
-            earnings: '€42.7',
+            earnings: '€13.29',
             location: {},
             onChapterEnter: [],
             onChapterExit: []
         },    
         {
             id: 'drive-slide-6',
-            title: '23.36: Rain and no more orders',
+            title: '17.35: So, I decided to take the tube',
             description: '<a target="_blank" href="../../riders/transcript/Ismael.txt"> Read Transcript </a> ',
             audio: '../../riders/audio/Ismael/7.m4a',
-            earnings: '€42.7',
+            earnings: '€18.68',
             location: {},
             onChapterEnter: [],
             onChapterExit: []
@@ -132,8 +149,8 @@ var config = {
         {
             id: 'slide-10',
             title: '',
-            earnings: '£61.07',
-            image: '../../riders/data/Lyon/Ismael.jpg',
+            earnings: '€18.68',
+            image: '../../riders/data/Lyon/Ismael Day 2.jpg',
             summaryzoom: 10,
             location: {
                 center: [-2.244644, 53.483959],
